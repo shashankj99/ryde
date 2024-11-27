e3474dad63af7f0d215eac676583f53748dbc90a
Shashank Jha <shashankj677@gmail.com>
2024-11-27 08:54:48 +0545

    added onboarding screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,62 @@
-import { Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
 
 export default function Welcome() {
+  const swiperRef = useRef<Swiper>(null);
+  const [actvieIndex, setActiveIndex] = useState<number>(0);
+  const isLastSlide = actvieIndex === onboarding.length - 1;
+
   return (
-    <SafeAreaView>
-      <Text>Welcome</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((o) => (
+          <View key={o.id} className="flex items-center justify-center p-5">
+            <Image
+              source={o.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="w-full flex flex-row items-center justify-center mt-10">
+              <Text className="text-black text-3xl text-center font-bold mx-10">
+                {o.title}
+              </Text>
+            </View>
+            <Text className="text-[#858585] text-lg text-center font-JakartaSemiBold mx-10 mt-3">
+              {o.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-10/12 mt-10 mb-5"
+      />
     </SafeAreaView>
   );
 }
---
+++ components/CustomButton.tsx
@@ -0,0 +1,57 @@
+import { ButtonProps } from "@/types/type";
+import { Text, TouchableOpacity } from "react-native";
+
+function getBgVariantStyle(variant: ButtonProps["bgVariant"]) {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286FF]";
+  }
+}
+
+function getTextVariantStyle(variant: ButtonProps["textVariant"]) {
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+    default:
+      return "text-white";
+  }
+}
+
+export default function CustomButton({
+  onPress,
+  title,
+  IconLeft,
+  IconRight,
+  className,
+  bgVariant = "primary",
+  textVariant = "default",
+  ...props
+}: ButtonProps) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className={`w-full p-3 rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      {...props}
+    >
+      {IconLeft && <IconLeft />}
+      <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+        {title}
+      </Text>
+      {IconRight && <IconRight />}
+    </TouchableOpacity>
+  );
+}
