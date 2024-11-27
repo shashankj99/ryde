44d60a3fbb37d5b2cab73a607f7f0d1be144c3d9
Shashank Jha <shashankj677@gmail.com>
2024-11-27 07:21:06 +0545

    added routes & pages for screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
-import { Text } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
-export default function index() {
-  return (
-    <SafeAreaView>
-      <Text>index</Text>
-    </SafeAreaView>
-  );
+export default function Home() {
+  return <Redirect href={"/(auth)/welcome"} />;
 }
