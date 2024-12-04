021d85ade2c4bfa6f277184d0f053fd9a558955a
Shashank Jha <shashankj677@gmail.com>
2024-12-04 08:55:58 +0545

    added screens to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import { Stack } from "expo-router";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 const Layout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="role" />
+      <Stack.Screen name="customer/auth" />
+      <Stack.Screen name="customer/home" />
+      <Stack.Screen name="captain/auth" />
+      <Stack.Screen name="captain/home" />
     </Stack>
   );
 };
 
-export default Layout;
+export default gestureHandlerRootHOC(Layout);
